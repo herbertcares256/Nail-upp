becdb05e90e0fbed85e298d781f597aebd34db4a
herbertcares256 <147391876+herbertcares256@users.noreply.github.com>
2026-08-11 00:49:25 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function bookHomeService() {
+  alert("Welcome to Nail Upp! Home service booking is coming next.");
+}
+
+function bookSalon() {
+  alert("Welcome to Nail Upp! Salon booking is coming next.");
+}
+
+function becomeTechnician() {
+  alert("Registration for nail technicians is coming next.");
+}
